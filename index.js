61f1b0ae95fe4da5037ef01470ead681408b4a59
Jordan Vera <verawebdevelopment@gmail.com>
2022-05-30 22:57:24 -0500

    relative path

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use('/', router);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('./build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.join('./build/index.html'));
+    res.sendFile('/build/index.html');
   });
 }
 
