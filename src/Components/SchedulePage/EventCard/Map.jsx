80e9432109d1695d8692aa437037275b83141020
Jordan Vera <verawebdevelopment@gmail.com>
2022-06-26 13:42:30 -0500

    changed google api key from ENV to config variable

--- src/Components/SchedulePage/EventCard/Map.jsx
+++ src/Components/SchedulePage/EventCard/Map.jsx
@@ -1,13 +1,14 @@
 import { useMemo } from 'react';
 import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
 import SpinnerCustom from '../../Spinner.js';
+import GOOGLE_MAPS_KEY from '../../../config/config.js';
 
 export default function Map({ latitude, longitude }) {
   const center = useMemo(() => ({ lat: latitude, lng: longitude }), []);
 
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_KEY,
+    googleMapsApiKey: GOOGLE_MAPS_KEY,
   });
 
   console.log('GOOOGLE API KEY', process.env.REACT_APP_GOOGLE_MAPS_KEY);
---
+++ src/config/config.js
@@ -0,0 +1,3 @@
+const GOOGLE_MAPS_KEY = 'AIzaSyCeh_anp603kFnnG9d2hgRL83BC-YebnkQ';
+
+export default GOOGLE_MAPS_KEY;
